50305beb241be30e4b47136989094bf63acd51a2
Kristoffer Röshammar <roshammar@gmail.com>
2018-10-12 18:36:21 +0200

    Fix bad variables in run_notebook

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run $(NET) --rm -i -t $(VOLUMES) $(ENV) $(PORTS) "$(NS)/$(REPO):$(VERSION)" sh -c "jupyter notebook --no-browser"
+docker run --net host --rm -i -t -v /root:/root -v /tmp:/tmp -w /root python-base sh -c "jupyter notebook --no-browser --allow-root"
